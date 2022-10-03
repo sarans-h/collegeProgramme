6d08c9c3bde674f02b3858154b75db40e30f0ba2
sarans-h <113707195+sarans-h@users.noreply.github.com>
2022-10-03 17:34:46 +0530

    Update col10.c

--- col10.c
+++ col10.c
@@ -1,11 +1,14 @@
 #include<stdio.h>
 int main(){
-    int d,w,y;
+    int d,w,y,k;
+    k=d;
     printf("Enter no of days:");
     scanf("%d",&d);
-    y=d/365;
-    w=d%365;
-    d=d-(y*365+w*7);
+    y=k/365;
+    k=k%365;
+    w=k/7;
+    k=k%7;
+    d=k;
     printf("YEARS=%d\nWEEKS=%d\nDAYS=%d",y,w,d);
     return 0;
 }
